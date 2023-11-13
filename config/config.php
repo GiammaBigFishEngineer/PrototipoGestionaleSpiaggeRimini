cf8927accd61dfd1d94c05f490587f326b32920b
Gianmaria Di Fronzo <64804734+GiammaBigFishEgineer@users.noreply.github.com>
2023-11-13 11:31:39 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,5 +5,5 @@ class Config
     static string $db_host = 'localhost';
     static string $db_name = 'Rimini';
     static string $db_username = 'root';
-    static string $db_password = 'topolino03';
+    static string $db_password = '';
 }
